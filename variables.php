13d5ba2f18ea68a71379baf7aa690ec820871ad8
m1g <miguelmalcolm@outlook.com>
2017-07-31 11:47:43 -0400

    initial commit

---
+++ variables.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+<?php
+
+  $name = "Miguel";
+  $number = 5; // integer
+  $Number_List = 100.5; // floats take more memory in php
+
+
+  echo $name . " " . $number;
+
+  $name = "<h1> HELLO </h1>"; // html tags and images can be assigned to variables one at a time
+
+  echo $name;
+
+?>
+
+
+</body>
+</html>
